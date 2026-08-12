c816d84d586b51bf2d589fe68d849b1e1a1b2d57
unknown <jmbcondados@gmail.com>
2026-08-12 10:17:10 -0300

    feat: add complete expense management application

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionForm extends StatefulWidget {
+  final void Function(String, double, DateTime) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.onSubmit(title, value, _selectedDate);
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titleController,
+              onSubmitted: (value) => _submitForm(),
+              decoration: InputDecoration(labelText: 'Título'),
+            ),
+            TextField(
+              controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (value) => _submitForm(),
+              decoration: InputDecoration(labelText: 'Valor (R\$)'),
+            ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'Nenhuma data selecionada!'
+                          : DateFormat('dd/MM/y').format(_selectedDate),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: _showDatePicker,
+                    child: Text(
+                      'Selecionar Data',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    _submitForm();
+                  },
+                  style: TextButton.styleFrom(foregroundColor: Colors.white),
+                  child: const Text('Nova Transação'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
